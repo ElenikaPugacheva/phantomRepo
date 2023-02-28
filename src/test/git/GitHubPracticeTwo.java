37920cb4551f2ffde04e76efce9b3af7f258e417
ElenikaPugacheva <126110777+ElenikaPugacheva@users.noreply.github.com>
2023-02-28 00:02:47 -0500

    Update GitHubPracticeTwo.java
    This changes are from upstream git repo

--- src/test/git/GitHubPracticeTwo.java
+++ src/test/git/GitHubPracticeTwo.java
@@ -4,4 +4,9 @@ public class GitHubPracticeTwo {
 	public int test(int a, int b) {
 		return a*b;
 	}
+	public int test(int x, inty, String message){
+		Sysytem.out.println(message);
+		int z = x*y;
+		return z;
+	}
 }
